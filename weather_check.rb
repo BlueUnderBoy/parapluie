8a83363766db54fe3519a61f7a18b18abb9ac36e
Jalen Young <120889301+BlueUnderBoy@users.noreply.github.com>
2024-09-07 08:33:53 +0000

    mettre a jour

--- weather_check.rb
+++ weather_check.rb
@@ -94,7 +94,7 @@ next_twelve_hours.each do |hour_hash|
   end
   chart = AsciiCharts::Cartesian.new(
     (0...next_twelve_hours.length),
-    precip_prob
+    Float(precip_prob)
     #:bar => true
   )
   puts chart.draw   
